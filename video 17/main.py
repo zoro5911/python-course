d340efd693215eaf1536cbfa354b56897915fb2e
zoro5911 <adityasura5911@gmail.com>
2024-09-16 21:55:17 +0530

    nothing

---
+++ video 17/main.py
@@ -0,0 +1,21 @@
+# name = 'Aditya'
+# for i in name:
+#   print(i)
+#   if(i =="i"):
+#     print("This is something special!")
+    
+# color = ["Cyan", "Gray", "voilet", "indigo"]
+# for color in color:
+#   print(color)
+#   for i in color:
+#     print(i)
+
+# for k in range(5):
+#   print(k + 1)
+  
+# for k in range(1, 2025):
+#   print(k)
+
+  
+for k in range(1, 32, 2):
+  print(k)
